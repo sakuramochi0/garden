2264b8226e7f2d7f682b3a22a1ee09a32bc3cbe3
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-07-31 13:54:23 +0900

    fix(posts): fix original URL links

--- mastodon/export-posts-to-content.fish
+++ mastodon/export-posts-to-content.fish
@@ -7,5 +7,5 @@ sqlite-utils query db.sqlite 'select * from statuses' | jq -c '.[]' | while read
 
    mkdir -p $basedir
 
-   echo $post | jq -r '"---\nupdated: " + .created_at + "\n---\n\n" + .content + "\n\n" + "&mdash; " + (.created_at | strptime("%Y-%m-%dT%H:%M:%S %Z") | mktime | strflocaltime("%Y-%m-%d %H:%M:%S %Z")) + "\n\n<!-- Original URL: https://mastodon.social/@sakuramochi0/" + (.id | tostring) + "-->"' > $basedir$filename
+   echo $post | jq -r '"---\nupdated: " + .created_at + "\n---\n\n" + .content + "\n\n" + "&mdash; " + (.created_at | strptime("%Y-%m-%dT%H:%M:%S %Z") | mktime | strflocaltime("%Y-%m-%d %H:%M:%S %Z")) + "\n\nOriginal URL: " + (.url)' > $basedir$filename
 end
