d2d3e8c3dcb4d4479fb3942742648b9970dbaa3e
sakuramochi <sakuramochi.mochi.0@gmail.com>
2025-01-12 20:49:44 +0900

    fix: exclude invalid post file and limit title to 40 characters

--- mastodon/export-posts-to-content.fish
+++ mastodon/export-posts-to-content.fish
@@ -5,13 +5,18 @@ sqlite-utils query db.sqlite 'select * from statuses' | jq -c '.[]' | while read
    set basedir ../content/posts/@sakuramochi0@mastodon-social/
    set filename (echo $post | jq -r '.url | split("/")[-1]').md
 
+   if [ $filename = "activity.md" ]
+     echo skip $filename
+     continue
+   end
+
    mkdir -p $basedir
 
    echo $post | jq -r '
    "---\n" +
    "created: " + .created_at + "\n" +
    "updated: " + .created_at + "\n" +
-   "title: \"" + (.content | sub("<[^>]*>"; ""; "g") | sub("\\\\\\\\"; "&#92;"; "g")[:80]) + "[...]\"\n" +
+   "title: \"" + (.content | sub("<[^>]*>"; ""; "g") | sub("\\\\\\\\"; "&#92;"; "g")[:40]) + "[...]\"\n" +
    "---\n\n" +
    .content + "\n\n" +
    "&mdash; " + (.created_at | strptime("%Y-%m-%dT%H:%M:%S %Z") | mktime | strflocaltime("%Y-%m-%d %H:%M:%S %Z")) + "\n\n" +
